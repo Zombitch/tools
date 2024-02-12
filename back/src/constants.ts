824edcb60e4598aa7b919ac2b4beb38189104643
Zombitch <ashbay.interactive@gmail.com>
2024-02-12 20:47:00 +0100

    Ansible : Improve script to add openai api key

--- back/src/constants.ts
+++ back/src/constants.ts
@@ -3,6 +3,6 @@ export const AppConfig = {
   encodingLoop: 5,
   cryptKey: "***Repl4ceCryptKeyValueForPr0dEnv.***",
   jwtSecret: "***Repl4ceValueForPr0dEnv.***",
-  openaiApiKey: "***Repl4ceOpenaiApiKeyValueForPr0dEnv.***"
+  openaiApiKey: "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"
 };
   
