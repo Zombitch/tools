b53586007a774266a3604325c3e2be8468891219
Zombitch <ashbay.interactive@gmail.com>
2025-01-17 23:54:56 +0100

    OpenAI : fix import issues

--- back/src/constants.ts
+++ back/src/constants.ts
@@ -1,9 +1,4 @@
 
 export const AppConfig = {
-  encodingLoop: 5,
-  cryptKey: "***Repl4ceCryptKeyValueForPr0dEnv.***",
-  jwtSecret: "***Repl4ceValueForPr0dEnv.***",
-  openaiApiKey: "8",
-  openaiCheckInterval: 1.5
 };
   
