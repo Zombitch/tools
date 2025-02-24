c41c7b372295031fe1fdf83ae2750c3ec2597192
Zombitch <ashbay.interactive@gmail.com>
2025-02-24 14:57:10 +0100

    Back - User : Prevent creation of a user if the username already exists

--- back/src/core/user/user.controller.ts
+++ back/src/core/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post, HttpCode, HttpStatus, BadRequestException } from '@nestjs/common';
+import { Controller, Body, Post, HttpCode, HttpStatus, BadRequestException, ForbiddenException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SecurityService } from '../security/security.service';
 import { AllowAnonymous } from '../auth/auth.guard';
@@ -14,12 +14,15 @@ export class UserController {
     @HttpCode(HttpStatus.OK)
     @Post()
     async create(@Body() body: Record<string, any>) {
-        if(!body.username || !body.password) throw new BadRequestException();
+        if(!body.username || !body.password) throw new BadRequestException("Empty values are not allowed.");
         let user: User = {
-            username: body.username,
-            password: await this.securityService.hash(body.password),
-            email: body?.email
+            username: body.username.toString(),
+            password: await this.securityService.hash(body.password.toString()),
+            email: body?.email.toString()
         }
+
+        const usernameDoesExist = await this.userService.findOneBy(body.username, "username");
+        if(usernameDoesExist) throw new ForbiddenException("Error while trying to create the account.");
         
         this.userService.insert(user, true);
     }
