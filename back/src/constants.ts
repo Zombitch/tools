89db00b8abdae5a4fb833de689b8351b937e3589
Zombitch <ashbay.interactive@gmail.com>
2025-02-28 10:57:14 +0100

    Ansible : Update node version Constant : Add mongo pwd

--- back/src/constants.ts
+++ back/src/constants.ts
@@ -5,6 +5,7 @@ export const AppConfig = {
   jwtSecret: "***Repl4ceValueForPr0dEnv.***",
   apiKey: "***Repl4ceValueApiKeyForPr0dEnv.***",
   openaiApiKey: "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",
-  openaiCheckInterval: 1.5
+  openaiCheckInterval: 1.5,
+  mongoPassword: "45125sszed?qZAEF"
 };
   
